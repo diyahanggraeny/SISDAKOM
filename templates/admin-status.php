ac6154c8d1faf378fac51f71f8febe32b8e0a02d
diyahanggraeny <62924671+diyahanggraeny@users.noreply.github.com>
2020-12-07 14:24:49 +0700

    Add files via upload

---
+++ templates/admin-status.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Status Participant</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="../static/css/main.css">
+    <link rel="stylesheet" href="../static/css//materialize.css">
+</head>
+
+<body class="blue">
+
+    <nav class="blue">
+        <div class="nav-wrapper">
+            <a></a>
+            <a href="#" class="brand-logo white-text" style="font-size: 25px;"><b>SISDAKOM</b></a>
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons black-text">menu</i></a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#" class="white-text" style="font-size: 20px;"><b>Dashboard</b></a></li>
+                <li><a href="#" class="white-text" style="font-size: 20px;"><b>Manage Admin</b></a></li>
+                <li><a href="#" class="white-text" style="font-size: 20px;"><b>Manage User</b></a></li>
+                <li><a href="#" class="white-text" style="font-size: 20px;"><b>Manage Event</b></a></li>
+            </ul>
+        </div>
+    </nav>
+
+    
+    <ul class="sidenav" id="mobile-demo">
+        <li><a href="#" class="black-text" >Dashboard</a></li>
+        <li><a href="#" class="black-text" >Manage Admin</a></li>
+        <li><a href="#" class="black-text" >Manage User</a></li>
+        <li><a href="#" class="black-text" >Manage Event</a></li>
+    </ul>
+
+
+    <main class="white">
+        <br>
+        <div class="container">
+            <h4 class="center typo-1"><b>STATUS</b></h4>
+            <form>
+                <div class="row">
+                    <div class="input-field col s12 m12 l12">
+                        <h5>Name</h5>
+                        <input id="nama_partisipan" type="text" class="validate">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12 m12 l12">
+                        <h5>Proof of Payment</h5>
+                        <input id="bukti_pembayaran" type="text" class="validate">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12 m12 l12">
+                        <h5>Status Payment</h5>
+                        <select>
+                            <option value="" disabled selected>Pilih Status</option>
+                            <option value="On Confirmation">On Confirmation</option>
+                            <option value="Paid">Paid</option>
+                            <option value="Done">Done</option>
+                            <option value="Cancel">Cancel</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s12 m12 l12">
+                        <button class="right btn waves-effect waves-light indigo" type="submit" name="action"><b>UPDATE STATUS</b>
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <br>
+    </main>
+
+    <footer class="blue">
+        <br>
+        <div class="center">
+            <a class="white-text"><b>Copyright © 2020 - Developed by Diyah, Izzat, Rachel</b></a>
+        </div>
+        <br>
+    </footer>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="../static/js/materialize.js"></script>
+    <script>
+        const sideNav = document.querySelectorAll('.sidenav');
+        M.Sidenav.init(sideNav);
+    </script>
+    <script>
+        $(document).ready(function(){
+        $('select').formSelect();
+        });
+    </script>
+</body>
+</html> 

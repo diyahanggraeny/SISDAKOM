197acf9cc475ead96c30a024f78b7159ae7865e6
diyahanggraeny <62924671+diyahanggraeny@users.noreply.github.com>
2020-12-23 09:53:28 +0700

    Add files via upload

--- templates/admin-login.php
+++ templates/admin-login.php
@@ -1,3 +1,39 @@
+<?php
+
+session_start();
+
+if( isset($_SESSION["login"])){
+    header("Location: admin-dashboard.php");
+    exit;
+}
+
+require 'functions2.php';
+
+if( isset($_POST["login"])) {
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $result = mysqli_query($conn, "SELECT * FROM admin WHERE admin_username = '$username'");
+
+    // cek username
+    if( mysqli_num_rows($result) == 1 ){
+        // cek password
+        $row = mysqli_fetch_assoc($result);
+        if( password_verify($password, $row["admin_password"]) ){
+            // set session
+            $_SESSION["login"] = $row["id_admin"];
+
+            header("Location: admin-dashboard.php");
+            exit;
+        }
+    }
+    $error = true;
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,22 +53,27 @@
                     <i class="center large material-icons icon-acc">account_circle</i>
                 </div>
             </div>
-            <form>
+
+            <?php if( isset($error) ): ?>
+                <p style="color:red; font-style:italic;" class="center">username / password salah</p>
+            <?php endif; ?>
+
+            <form action="" method="post">
                 <div class="row">
                     <div class="input-field col s12 m6 offset-m3 l6 offset-l3">
-                        <input id="username" type="text" class="validate">
+                        <input id="username" name="username" type="text" class="validate">
                         <label for="username">Username</label>
                     </div>
                 </div>
                 <div class="row">
                     <div class="input-field col s12 m6 offset-m3 l6 offset-l3">
-                        <input id="password" type="password" class="validate">
+                        <input id="password" name="password" type="password" class="validate">
                         <label for="password">Password</label>
                     </div>
                 </div>
                 <div class="row">
                     <div class="center col s12 m6 offset-m3 l6 offset-l3">
-                        <button class="btn waves-effect waves-light black-text" type="submit" name="action"><b>LOGIN</b>
+                        <button class="btn waves-effect waves-light black-text" type="submit" name="login"><b>LOGIN</b>
                         </button>
                     </div>
                 </div>

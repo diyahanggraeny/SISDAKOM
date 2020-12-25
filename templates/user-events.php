c146caf52126ec16b5883f76df3141e18c415cc8
rachelharyawan <62920741+rachelharyawan@users.noreply.github.com>
2020-12-26 02:33:34 +0700

    Update user-events.php

--- templates/user-events.php
+++ templates/user-events.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+require 'functions2.php';
+
+if (!isset($_SESSION["loginsubmit"])) {
+    header("Location: login.php");
+    exit;
+}
+
+$events = query("SELECT * FROM event");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,105 +29,43 @@
             <a href="#" class="brand-logo white-text" style="font-size: 25px;"><b>SISDAKOM</b></a>
             <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons black-text">menu</i></a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="#" class="white-text" style="font-size: 20px;"><b>Home</b></a></li>
-                <li><a href="#" class="white-text" style="font-size: 20px;"><b>Events</b></a></li>
-                <li><a href="#" class="white-text" style="font-size: 20px;"><b>Messages</b></a></li>
-                <li><a href="#" class="white-text" style="font-size: 20px;"><b>Profile</b></a></li>
+                <li><a href="user-home.php" class="white-text" style="font-size: 20px;"><b>Home</b></a></li>
+                <li><a href="user-events.php" class="white-text" style="font-size: 20px;"><b>Events</b></a></li>
+                <li><a href="user-message.php" class="white-text" style="font-size: 20px;"><b>Messages</b></a></li>
+                <li><a href="user-profile-info.php" class="white-text" style="font-size: 20px;"><b>Profile</b></a></li>
             </ul>
         </div>
     </nav>
 
     
     <ul class="sidenav" id="mobile-demo">
-        <li><a href="#" class="black-text">Home</a></li>
-        <li><a href="#" class="black-text">Events</a></li>
-        <li><a href="#" class="black-text">Messages</a></li>
-        <li><a href="#" class="black-text">Profile</a></li>
+        <li><a href="user-home.php" class="black-text">Home</a></li>
+        <li><a href="user-events.php" class="black-text">Events</a></li>
+        <li><a href="user-message.php" class="black-text">Messages</a></li>
+        <li><a href="user-profile-info.php" class="black-text">Profile</a></li>
     </ul>
 
 
     <main class="main">
-        <div class="row">
-            <div class="col l2 s12" style="margin-left: 100px; margin-top: 25px;">
-                <div class="card border1 center-align">
-                    <img class="image4" src="../static/img/logo-woc.png">
-                    <a class="blue-text font3"><b>Words of Courage</b></a>
-                    <a class="blue-text font4 left"><b>20 September 2020</b></a>
-                    <br>
-                    <a class="blue-text font4 left"><b>Zoom Meeting</b></a>
-                    <br>
-                    <br>
-                    <a class="blue-text font4 left"><b>Free</b></a>
-                    <br>
-                    <a href="#" class="blue btn white-text" style="margin-top: 10px; width: 100px; border-radius: 10px;"><b>Details</b></a>
-                    <br>
-                    <a href="#" class="blue btn white-text" style="margin-top: 5px; margin-bottom: 10px; width: 100px; border-radius: 10px;"><b>Register</b></a>
-                </div>
-            </div>
+        <div class="row" style="margin-left: 100px;">
+        <?php foreach($events as $row) : ?>
             <div class="col l2 s12" style="margin-top: 25px;">
                 <div class="card border1 center-align">
-                    <img class="image4" src="../static/img/logo-thewalls.png">
-                    <a class="blue-text font3"><b>The Walls</b></a>
-                    <a class="blue-text font4 left"><b>25 September 2020</b></a>
+                    <img src="../static/img/<?= $row["poster_event"] ?>" style="margin-top: 5px" width="170px" height="250px">
+                    <a class="blue-text font3"><b><?= $row["nama_event"]; ?></b></a>
+                    <br><br>
+                    <a class="blue-text font4 center"><b><?= $row["tanggal_event"]; ?></b></a>
                     <br>
-                    <a class="blue-text font4 left"><b>GDS 608, UNJ</b></a>
+                    <a class="blue-text font4 center"><b><?= $row["tempat_event"]; ?></b></a>
                     <br>
+                    <a class="blue-text font4 center">Rp <?= $row["htm"]; ?></a>
+                    <br><br>
+                    <a href="user-eventdetail.php" class="blue btn white-text" style="margin-top: 10px; width: 100px; border-radius: 10px;"><b>Details</b></a>
                     <br>
-                    <a class="blue-text font4 left"><b>Rp50.000</b></a>
-                    <br>
-                    <a href="#" class="blue btn white-text" style="margin-top: 10px; width: 100px; border-radius: 10px;"><b>Details</b></a>
-                    <br>
-                    <a href="#" class="blue btn white-text" style="margin-top: 5px; margin-bottom: 10px; width: 100px; border-radius: 10px;"><b>Register</b></a>
+                    <a href="user-register-and-payment.php" class="blue btn white-text" style="margin-top: 5px; margin-bottom: 10px; width: 100px; border-radius: 10px;"><b>Register</b></a>
                 </div>
             </div>
-            <div class="col l2 s12" style="margin-top: 25px;">
-                <div class="card border1 center-align">
-                    <img class="image4" src="../static/img/logo-zadira.png">
-                    <a class="blue-text font3"><b>Zadira Mart</b></a>
-                    <a class="blue-text font4 left"><b>10 Oktober 2020</b></a>
-                    <br>
-                    <a class="blue-text font4 left"><b>GDS 607, UNJ</b></a>
-                    <br>
-                    <br>
-                    <a class="blue-text font4 left"><b>Rp20.000</b></a>
-                    <br>
-                    <a href="#" class="blue btn white-text" style="margin-top: 10px; width: 100px; border-radius: 10px;"><b>Details</b></a>
-                    <br>
-                    <a href="#" class="blue btn white-text" style="margin-top: 5px; margin-bottom: 10px; width: 100px; border-radius: 10px;"><b>Register</b></a>
-                </div>
-            </div>
-            <div class="col l2 s12" style="margin-top: 25px;">
-                <div class="card border1 center-align">
-                    <img class="image4" src="../static/img/logo-shopofcourage.png">
-                    <a class="blue-text font3"><b>Shop of Courage</b></a>
-                    <a class="blue-text font4 left"><b>24 Oktober 2020</b></a>
-                    <br>
-                    <a class="blue-text font4 left"><b>GDS 507, UNJ</b></a>
-                    <br>
-                    <br>
-                    <a class="blue-text font4 left"><b>Rp35.000</b></a>
-                    <br>
-                    <a href="#" class="blue btn white-text" style="margin-top: 10px; width: 100px; border-radius: 10px;"><b>Details</b></a>
-                    <br>
-                    <a href="#" class="blue btn white-text" style="margin-top: 5px; margin-bottom: 10px; width: 100px; border-radius: 10px;"><b>Register</b></a>
-                </div>
-            </div>
-            <div class="col l2 s12" style="margin-top: 25px;">
-                <div class="card border1 center-align">
-                    <img class="image4" src="../static/img/logo-thewalls.png">
-                    <a class="blue-text font3"><b>The Walls Seminar</b></a>
-                    <a class="blue-text font4 left"><b>03 November 2020</b></a>
-                    <br>
-                    <a class="blue-text font4 left"><b>GDS 505, UNJ</b></a>
-                    <br>
-                    <br>
-                    <a class="blue-text font4 left"><b>Rp100.000</b></a>
-                    <br>
-                    <a href="#" class="blue btn white-text" style="margin-top: 10px; width: 100px; border-radius: 10px;"><b>Details</b></a>
-                    <br>
-                    <a href="#" class="blue btn white-text" style="margin-top: 5px; margin-bottom: 10px; width: 100px; border-radius: 10px;"><b>Register</b></a>
-                </div>
-            </div>
+        <?php endforeach; ?>
         </div>
 
         <div class="row">

0690460d84e33a7f928354c85bbe1d009f09805f
izzatibrahim27 <62867632+izzatibrahim27@users.noreply.github.com>
2020-12-26 00:55:52 +0700

    Add files via upload

--- templates/user-profile-info.php
+++ templates/user-profile-info.php
@@ -1,3 +1,18 @@
+<?php
+session_start();
+
+if( !isset($_SESSION["login"])){
+    header("Location: login.php");
+    exit;
+}
+
+require 'functions2.php';
+
+$id_user = $_SESSION["login"];
+$result = mysqli_query($conn, "SELECT * FROM user WHERE id_user = '$id_user'");
+
+
+?>
 <!DOCTYPE html>
   <html>
     <head>
@@ -19,49 +34,55 @@
               <a href="#" class="brand-logo white-text" style="font-size: 25px;"><b>SISDAKOM</b></a>
               <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons black-text">menu</i></a>
               <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="#" class="white-text" style="font-size: 20px;"><b>Home</b></a></li>
-                <li><a href="#" class="white-text" style="font-size: 20px;"><b>Events</b></a></li>
-                <li><a href="#" class="white-text" style="font-size: 20px;"><b>Messages</b></a></li>
-                <li><a href="#" class="white-text" style="font-size: 20px;"><b>Profile</b></a></li>
+                <li><a href="index.php" class="white-text" style="font-size: 20px;"><b>Home</b></a></li>
+                <li><a href="user-events.php" class="white-text" style="font-size: 20px;"><b>Events</b></a></li>
+                <li><a href="user-message.php" class="white-text" style="font-size: 20px;"><b>Messages</b></a></li>
+                <li><a href="user-profile-info.php" class="white-text" style="font-size: 20px;"><b>Profile</b></a></li>
               </ul>
             </div>
         </nav>
 
         <ul class="sidenav" id="mobile-demo">
-            <li><a href="#" class="black-text">Home</a></li>
-            <li><a href="#" class="black-text">Events</a></li>
-            <li><a href="#" class="black-text">Message</a></li>
-            <li><a href="#" class="black-text">Profile</a></li>
+            <li><a href="index.php" class="black-text">Home</a></li>
+            <li><a href="user-events.php" class="black-text">Events</a></li>
+            <li><a href="user-message.php" class="black-text">Message</a></li>
+            <li><a href="user-profile-info.php" class="black-text">Profile</a></li>
         </ul>
 
 <!--Main Page-->
 <div class="container">
   <div class="row">
+    
       <div class="col s3 card-panel blue lighten-4">
+        <?php while( $row = mysqli_fetch_assoc($result) ) : ?>
           <img class="responsive-img" src="img/profile.png" style="margin-top: 20px;">
-          <h5 class="blue-text bold center">Rose Azalea</h5>
+          <h5 class="blue-text bold center"><?= $row["full_name"]; ?></h5>
           <div class="collection">
-            <a href="#!" class="collection-item active white-text blue">Account Information</a>
-            <a href="#!" class="collection-item white-text blue">My Events</a>
-            <a href="#!" class="collection-item white-text blue">Changed Password</a>
+            <a href="user-profile-info.php" class="collection-item active white-text blue">Account Information</a>
+            <a href="user-profile-book-event.php" class="collection-item white-text blue">My Events</a>
+            <a href="user-profile-changed-password.php" class="collection-item white-text blue">Changed Password</a>
           </div>
             <div class="center">
-              <a class="white-text btn blue" style="margin-bottom: 20px;">Logout</a>
+              <a href="index.php" class="white-text btn blue" style="margin-bottom: 20px;">Logout</a>
             </div>
+            
       </div>
-      <div class="col s9">
+      <div class="col s9"> 
+        
         <h5 class="blue-text" style="margin-top: 30px; margin-left: 20px;">Full Name</h5>
-        <h5 class="black-text" style="margin-top: 10px; margin-left: 20px;">Rose Azalea</h5>
+        <h5 class="black-text" style="margin-top: 10px; margin-left: 20px;"><?= $row["full_name"]; ?></h5>
         <h5 class="blue-text" style="margin-top: 10px; margin-left: 20px;">Email</h5>
-        <h5 class="black-text" style="margin-top: 10px; margin-left: 20px;">roseazalea@mail.com</h5>
+        <h5 class="black-text" style="margin-top: 10px; margin-left: 20px;"><?= $row["user_email"]; ?></h5>
         <h5 class="blue-text" style="margin-top: 10px; margin-left: 20px;">Username</h5>
-        <h5 class="black-text" style="margin-top: 10px; margin-left: 20px;">roseazalea</h5>
+        <h5 class="black-text" style="margin-top: 10px; margin-left: 20px;"><?= $row["user_username"]; ?></h5>
         <h5 class="blue-text" style="margin-top: 10px; margin-left: 20px;">Instansi</h5>
-        <h5 class="black-text" style="margin-top: 10px; margin-left: 20px;">Universitas Negeri Jakarta</h5>
+        <h5 class="black-text" style="margin-top: 10px; margin-left: 20px;"><?= $row["instansi"]; ?></h5>
         <h5 class="blue-text" style="margin-top: 10px; margin-left: 20px;">Year</h5>
-        <h5 class="black-text" style="margin-top: 10px; margin-left: 20px;">2018</h5>
-        <a class="center white-text btn blue" style="margin-top: 30px;">Edit Profile</a>
+        <h5 class="black-text" style="margin-top: 10px; margin-left: 20px;"><?= $row["angkatan"]; ?></h5>
+        <a class="center white-text btn indigo" href="user-profile-edit.php?id=<?= $row["id_user"]; ?>" ><b>Edit Profile</b></a>
+        <?php endwhile; ?>
       </div>
+      
     </div>
 </div>
 

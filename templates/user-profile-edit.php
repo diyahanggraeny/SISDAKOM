712d3d3cf2ba2d47482cddf9e0e9ee1e4812988c
izzatibrahim27 <62867632+izzatibrahim27@users.noreply.github.com>
2020-12-27 22:57:14 +0700

    Add files via upload

--- templates/user-profile-edit.php
+++ templates/user-profile-edit.php
@@ -1,3 +1,41 @@
+<?php 
+session_start();
+
+if( !isset($_SESSION["loginsubmit"])){
+    header("Location: login.php");
+    exit;
+}
+
+require 'functions2.php';
+
+// cek tombol submit sudah ditekan atau belum
+if( isset($_POST["submit"]) ){
+    // cek apakah data berhasil ubah atau tidak
+    if ( ubahuser($_POST) > 0 ) {
+        echo "
+            <script>
+                alert('Data berhasil diubah!');
+                document.location.href = 'user-profile-info.php';
+            </script>
+        
+        ";
+    } else {
+        echo "
+            <script>
+                alert('Data gagal diubah!');
+                document.location.href = 'user-profile-edit.php';
+            </script>
+    
+    "; }
+}
+
+$id = $_GET["id"];
+
+$mhs = query("SELECT * FROM user WHERE id_user = $id")[0];
+
+
+?>
+
 <!DOCTYPE html>
   <html>
     <head>
@@ -11,68 +49,75 @@
       <title>Profile - Edit</title>
     </head>
 
-    <body>
-
-      <!--Navbar-->
-         <nav class="light-blue lighten-1" role="navigation">
-            <div class="nav-wrapper container">
-              <a href="#" class="brand-logo white-text" style="font-size: 25px;"><b>SISDAKOM</b></a>
-              <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons black-text">menu</i></a>
-              <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="#" class="white-text" style="font-size: 20px;"><b>Home</b></a></li>
-                <li><a href="#" class="white-text" style="font-size: 20px;"><b>Events</b></a></li>
-                <li><a href="#" class="white-text" style="font-size: 20px;"><b>Messages</b></a></li>
-                <li><a href="#" class="white-text" style="font-size: 20px;"><b>Profile</b></a></li>
-              </ul>
-            </div>
-        </nav>
-
-        <ul class="sidenav" id="mobile-demo">
-            <li><a href="#" class="black-text">Home</a></li>
-            <li><a href="#" class="black-text">Events</a></li>
-            <li><a href="#" class="black-text">Message</a></li>
-            <li><a href="#" class="black-text">Profile</a></li>
-        </ul>
+    <body class="white">
+<!--navbar-->
+      <nav class="blue">
+        <div class="nav-wrapper">
+            <a><img class="first-logo" src="../static/img/profile.png"></a>
+            <a href="#" class="brand-logo white-text" style="font-size: 25px;"><b>SISDAKOM</b></a>
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons black-text">menu</i></a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="user-home.php" class="white-text" style="font-size: 20px;"><b>Home</b></a></li>
+                <li><a href="user-events.php" class="white-text" style="font-size: 20px;"><b>Events</b></a></li>
+                <li><a href="user-message.php" class="white-text" style="font-size: 20px;"><b>Messages</b></a></li>
+                <li><a href="user-profile-info.php" class="white-text" style="font-size: 20px;"><b>Profile</b></a></li>
+            </ul>
+        </div>
+    </nav>
+    
+    <ul class="sidenav" id="mobile-demo">
+        <li><a href="user-home.php" class="black-text">Home</a></li>
+        <li><a href="user-events.php" class="black-text">Events</a></li>
+        <li><a href="user-message.php" class="black-text">Messages</a></li>
+        <li><a href="user-profile-info.php" class="black-text">Profile</a></li>
+    </ul>
 
 <!--Main Page-->
 <div class="container">
   <div class="row">
       <div class="col s3 card-panel blue lighten-4">
-          <img class="responsive-img" src="img/profile.png" style="margin-top: 20px;">
-          <h5 class="blue-text bold center">Rose Azalea</h5>
+          
           <div class="collection">
-            <a href="#!" class="collection-item active white-text blue">Account Information</a>
-            <a href="#!" class="collection-item white-text blue">My Events</a>
-            <a href="#!" class="collection-item white-text blue">Changed Password</a>
+            <a href="user-profile-info.php" class="collection-item active white-text blue">Account Information</a>
+            <a href="user-profile-book-event.php" class="collection-item white-text blue">My Events</a>
+            <a href="user-profile-changed-password.php" class="collection-item white-text blue">Changed Password</a>
           </div>
             <div class="center">
-              <a class="white-text btn blue" style="margin-bottom: 20px;">Logout</a>
+              <a href="logout.php" class="white-text btn blue" style="margin-bottom: 20px;">Logout</a>
             </div>
       </div>
-      <div class="col s9">
+      <div class="input-field col s9">
+        <form action="" method="post" enctype="multipart/form-data">
         <div class="row">
+          <input  name="id_user" type="hidden" value="<?= $mhs["id_user"]; ?>">
           <div class="input-field col s6" style="margin-top: 15px; margin-left: 30px;">
-            <input value="Rose Azalea" id="fullname" type="text" class="validate">
+            <input id="full_name" name="full_name" type="text" class="validate" value="<?= $mhs["full_name"]; ?>" required>
             <label class="active" for="fullname">Full Name</label>
           </div>
+
           <div class="input-field col s6" style="margin-left: 30px;">
-            <input value="roseazalea@mail.com" id="email" type="email" class="validate">
+            <input id="user_email" name="user_email" type="email" class="validate" value="<?= $mhs["user_email"]; ?>" required>
             <label class="active" for="email">E-mail</label>
           </div>
+
           <div class="input-field col s6" style="margin-left: 30px;">
-            <input value="roseazalea" id="username" type="text" class="validate">
+            <input id="user_username" name="user_username" type="text" class="validate" value="<?= $mhs["user_username"]; ?>" required>
             <label class="active" for="username">Username</label>
           </div>
+
           <div class="input-field col s6" style="margin-left: 30px;">
-            <input value="Universitas Negeri Jakarta" id="instansi" type="text" class="validate">
+            <input id="instansi" name="instansi" type="text" class="validate" value="<?= $mhs["instansi"]; ?>" required>
             <label class="active" for="instansi">Instansi</label>
           </div>
+
           <div class="input-field col s6" style="margin-left: 30px;">
-            <input value="2018" id="year" type="text" class="validate">
+            <input id="angkatan" name="angkatan" type="text" class="validate" value="<?= $mhs["angkatan"]; ?>" required>
             <label class="active" for="year">Year</label>
           </div>
+
         </div>
-        <a class="center white-text btn blue" style="margin-left: 30px;">Save</a>
+        <button class="center btn waves-effect waves-light indigo" type="submit" name="submit"><b>Save</b>
+        </button>
       </div>
     </div>
 </div>
@@ -83,7 +128,7 @@
     <div class="container">
       <div class="row">
         <div class="col l4 s12">
-          <h5 class="white-text">CAbout</h5>
+          <h5 class="white-text">About</h5>
           <p class="white-text">A website that provide platform for Computer Science events on State University of Jakarta</p>
         </div>
         <div class="col l4 s12">

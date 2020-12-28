9410304d7ffc6be0252c98f8f0444c15562e6023
rachelharyawan <62920741+rachelharyawan@users.noreply.github.com>
2020-12-29 02:14:30 +0700

    Update admin-logout.php

--- templates/admin-logout.php
+++ templates/admin-logout.php
@@ -1,12 +1,14 @@
 <?php
 
 session_start();
-
+$_SESSION = [];
 session_unset();
 session_destroy();
 
-header("Location: admin-login.php");
-exit;
+setcookie('id', '', time()-3600);
+setcookie('key', '', time()-3600);
 
+header("Location: login.php");
+exit;
 
 ?>

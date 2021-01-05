19fe22db313162afcad61df9a058980ec1569002
rachelharyawan <62920741+rachelharyawan@users.noreply.github.com>
2021-01-05 09:41:35 +0700

    Update user-finish-event.php

--- templates/user-finish-event.php
+++ templates/user-finish-event.php
@@ -8,13 +8,17 @@ if( !isset($_SESSION["loginsubmit"])){
 
 require 'functions2.php';
 
-$idevent = $_GET["idevent"];
-$result = mysqli_query($conn, "SELECT * FROM event WHERE id_event = '$idevent'");
+$id_user = $_SESSION["loginsubmit"];
+$result = mysqli_query($conn, "SELECT * FROM user WHERE id_user = '$id_user'");
 
-if (!isset($idevent)){
-  header("Location: guest-events.php");
-    exit;
-}
+$events = query("SELECT * FROM event");
+
+$eventbayar = query("SELECT * FROM event_partisipan_bayar
+                  INNER JOIN event ON event_partisipan_bayar.id_event = event.id_event WHERE status_event = 'Done'");
+
+$eventgratis = query("SELECT * FROM event_partisipan_gratis
+                  INNER JOIN event ON event_partisipan_gratis.id_event = event.id_event WHERE status_event = 'Done'");
+
 ?>
 
 <!DOCTYPE html>
@@ -23,11 +27,11 @@ if (!isset($idevent)){
       <!--Import Google Icon Font-->
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="../static/css//materialize.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="../static/css//materialize.css" media="screen,projection"/>
 
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <title>Finish Event Details</title>
+      <title>Profile - My Events (Finish)</title>
     </head>
 
     <body class="white">
@@ -54,32 +58,63 @@ if (!isset($idevent)){
         <li><a href="user-profile-info.php" class="black-text">Profile</a></li>
     </ul>
 
-<!--Halaman Utama-->
+<!--Main Page-->
 <div class="container">
-  <?php while ($row = mysqli_fetch_assoc($result)): ?>
   <div class="row">
-    <div class="col s12 l3">
-      <img class="responsive-img" src="../static/img/<?= $row['poster_event'];?>" style="margin-top: 20px;">
+      <div class="col s12 l3 card-panel blue lighten-4 center-align">
+        <?php while( $row = mysqli_fetch_assoc($result) ) : ?>
+          <img class="responsive-img" src="../static/img/profile.png" style="margin-top: 20px;">
+          <h5 class="blue-text bold center"><?= $row["full_name"]; ?></h5>
+          <div class="collection">
+            <a href="user-profile-info.php" class="collection-item white-text blue">Account Information</a>
+            <a href="user-profile-book-event.php" class="collection-item active white-text blue">My Events</a>
+            <a href="user-profile-changed-password.php" class="collection-item white-text blue">Changed Password</a>
+          </div>
+            <div class="center">
+              <a href="logout.php" class="white-text btn blue" style="margin-bottom: 20px;">Logout</a>
+            </div>
+            <?php endwhile; ?>
+      </div>
+      <div class="col s12 l9">
+          <ul class="tabs" style="margin-top: 15px;">
+            <li class="tab col s12 l3"><a href="user-profile-book-event.php" class="blue-text">Booked</a></li>
+            <li class="tab col s12 l3 blue"><a href="user-profile-finish-event.php" class="white-text">Finished</a></li>
+          </ul>
+          <?php foreach($eventbayar as $event) : ?>
+            <?php if( $event['id_user'] == $id_user ){?>
+          <div class="col s12 l4">
+            <img class="responsive-img" src="../static/img/<?= $event['poster_event'];?>" style="margin-top: 20px; margin-left: 20px;">
+          </div>
+          <div class="col s12 l8" style="margin-bottom: 100px">
+            <h5 class="blue-text" style="margin-left: 10px;"><b><?= $event['nama_event'];?></b></h5>
+            <h5 class="blue-text" style="margin-left: 10px;"><?= $event['tanggal_event'];?></h5>
+            <h5 class="blue-text" style="margin-top: 0px; margin-left: 10px;"><?= $event['tempat_event'];?></h5>
+            <h5 class="blue-text" style="margin-top: 0px; margin-left: 10px;"><?= $event['waktu_event'];?></h5>
+            <a href="user-book-event-detail.php?id=<?= $event['id_event'];?>" class="center white-text btn blue" style="margin-top: 0px; margin-left: 10px;">Details</a>
+          </div>
+            <?php } ?>
+          <?php endforeach; ?>
+          <?php foreach($eventgratis as $event2) : ?>
+            <?php if( $event2['id_user'] == $id_user ){?>
+          <div class="col s12 l4">
+            <img class="responsive-img" src="../static/img/<?= $event2['poster_event'];?>" style="margin-top: 20px; margin-left: 20px;">
+          </div>
+          <div class="col s12 l8" style="margin-bottom: 100px">
+            <h5 class="blue-text" style="margin-left: 10px;"><b><?= $event2['nama_event'];?></b></h5>
+            <h5 class="blue-text" style="margin-left: 10px;"><?= $event2['tanggal_event'];?></h5>
+            <h5 class="blue-text" style="margin-top: 0px; margin-left: 10px;"><?= $event2['tempat_event'];?></h5>
+            <h5 class="blue-text" style="margin-top: 0px; margin-left: 10px;"><?= $event2['waktu_event'];?></h5>
+            <a href="user-book-event-detail.php?id=<?= $event2['id_event'];?>" class="center white-text btn blue" style="margin-top: 0px; margin-left: 10px;">Details</a>
+          </div>
+            <?php } ?>
+          <?php endforeach; ?>
+          </div>
+      </div>
     </div>
-    <div class="col s12 l7">
-      <h4 class="blue-text bold"><?= $row['nama_event'];?></h4>
-      <h5 class="blue-text bold" style="font-size: 20px; margin-top: 20px;"><?= $row['tanggal_event'];?></h5>
-      <h5 class="blue-text bold" style="font-size: 20px;"><?= $row['waktu_event'];?></h5>
-      <h5 class="blue-text bold" style="font-size: 20px;"><?= $row['tempat_event'];?></h5>
-    </div>
-    <div class="col s12 l2">
-      <a href="#" class="white-text btn-large blue" style="margin-top: 25px; width: 300px;">Download E-Certificate</a>
-    </div>
-    <div class="col s12 l12">
-      <p class="black-text bold" style="font-size: 20px;">
-      <?= $row['informasi_event'];?>
-      </p>
-    </div>
-  </div>
-  <?php endwhile; ?>
 </div>
 
-<!--Footer--> 
+
+        <!--Footer--> 
   <footer class="page-footer blue">
     <div class="container">
       <div class="row">
@@ -108,10 +143,13 @@ if (!isset($idevent)){
   </footer>
 
       <!--JavaScript at end of body for optimized loading-->
-      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script type="text/javascript" src="../static/js/materialize.js"></script>
       <script>
         const sideNav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sideNav);
       </script> 
+      <script>
+        var instance = M.Tabs.init(el, options);
+      </script>
     </body>
   </html>

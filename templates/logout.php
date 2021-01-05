68ff57be05a6e816d14ab9a868f3a248fde2e332
rachelharyawan <62920741+rachelharyawan@users.noreply.github.com>
2021-01-05 19:55:07 +0700

    Update logout.php

--- templates/logout.php
+++ templates/logout.php
@@ -1,6 +1,14 @@
 <?php
-
 session_start();
+require 'functions2.php';
+
+$id_user = $_SESSION["loginsubmit"];
+$result = mysqli_query($conn, "SELECT * FROM user WHERE id_user = '$id_user'");
+$row = mysqli_fetch_assoc($result);
+
+$log =  $row["user_username"] .= " logout dari sistem" ;
+            act_log($log);
+
 $_SESSION = [];
 session_unset();
 session_destroy();

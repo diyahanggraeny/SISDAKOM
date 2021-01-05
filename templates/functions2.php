c037bb6630496f62381daee26e2f25bffc5802d3
diyahanggraeny <62924671+diyahanggraeny@users.noreply.github.com>
2021-01-06 05:55:22 +0700

    Update functions2.php

--- templates/functions2.php
+++ templates/functions2.php
@@ -22,6 +22,13 @@ function hapus($id) {
     return mysqli_affected_rows($conn);
 }
 
+function hapususer($id) {
+    global $conn;
+    mysqli_query($conn, "DELETE FROM user WHERE id_user = $id");
+
+    return mysqli_affected_rows($conn);
+}
+
 
 function hapusevent($id) {
     global $conn;
